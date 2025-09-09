78cba22d0fc18aaea46336584c96b95f75c0bfe3
kmjak <tymkoron1221@icloud.com>
2025-09-09 18:09:17 +0900

    feat: PressReleaseの型を定義

---
+++ frontend/src/features/pressreleases/shared/types/PressRelease.ts
@@ -0,0 +1,16 @@
+/**
+ * @property {number} id - PressRelease ID
+ * @property {string} title - タイトル
+ * @property {string} content - 内容
+ * @property {string} created_at - 作成日時
+ * @property {string} updated_at - 更新日時
+ * @property {number} userId - ユーザーID
+ */
+export type PressReleaseType = {
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+  userId: number;
+};
